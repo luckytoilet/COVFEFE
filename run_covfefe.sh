c405573a1325d32e4c64b60b1ce02b99b8fb6b2d
Bai Li <bai@cs.toronto.edu>
2018-10-17 16:23:34 -0400

    Configuration files for dementiabank extraction

---
+++ run_covfefe.sh
@@ -0,0 +1,12 @@
+# For testing lex features
+#python covfefe.py -i ~/research/lex_test -o ~/research/lex_out -p lex
+
+# For processing dementiabank
+#python covfefe.py -i ~/data/dementiabank_txt/Controls -o ~/research/dementiabank_features/Controls -p lex -n 16
+#python covfefe.py -i ~/data/dementiabank_txt/Dementia -o ~/research/dementiabank_features/Dementia -p lex -n 16
+
+# For processing google translated mandarin
+#python covfefe.py -i ~/research/bai-alzheimer/data/lu_google_translated/ -o ~/data/mandarin_translated_features/ -p lex -n 16
+
+# For processing opensubtitles pieces
+python covfefe.py -i /h/bai/moar/os_narrations_en/ -o /h/bai/moar/os_features_en/ -p lex -n 16
