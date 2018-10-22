73a3414fae14950bd2482060d25a0e7e5df32833
Bai Li <bai@cs.toronto.edu>
2018-10-22 01:35:19 -0400

    Emit CSV file for multilingual lex, no features emitted yet

--- nodes/lexicosyntactic.py
+++ nodes/lexicosyntactic.py
@@ -2,6 +2,8 @@ import logging
 import os
 import datetime
 import subprocess
+import collections
+import csv
 
 from nodes.helper import FileOutputNode
 from utils import file_utils
@@ -156,6 +158,7 @@ def load_conf(config_file):
 class MultilingualLex(FileOutputNode):
     def setup(self):
         self.output_parse_dir = os.path.join(self.out_dir, "stanford_parses")
+        self.features = collections.OrderedDict()
         
     def _run_chinese_corenlp(self, filepath):
         # lexparser_chinese.sh [output_dir] [transcript_file]
@@ -165,9 +168,19 @@ class MultilingualLex(FileOutputNode):
             filepath
         ])
 
+    def _write_features(self, out_file):
+        with open(out_file, 'w') as f:
+            csvw = csv.writer(f)
+            csvw.writerow(list(self.features.keys()))
+            csvw.writerow(list(self.features.values()))
+
     def run(self, filepath):
         self.log(logging.INFO, "Starting %s" % (filepath))
         out_file = self.derive_new_file_path(filepath, ".csv")
 
         if file_utils.should_run(filepath, out_file):
+            self.features['FileID'] = filepath
             self._run_chinese_corenlp(filepath)
+            self._write_features(out_file)
+
+        self.emit(out_file)
